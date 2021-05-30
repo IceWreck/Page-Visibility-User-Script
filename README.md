a3fed529abb8bd166cecea51e5f527518f23884e
Anchit Bajaj <ab@abifog.com>
2021-05-30 21:59:40 +0530

    Also try to block the blur/focus APIs

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Page-Visibility-User-Script
-Prevent websites from knowing if you have opened a new tab/window or using a different application, etc.
-.
+Prevent websites from knowing if you have opened a new tab/window or if you are using a different application, etc. This userscript blocks the page visibility API and to some extent the old blur/focus APIs.
+
+Example: Twitter stops playing videos as soon as you switch tabs. This userscript prevents that.
+
 ## Instructions
-* Install a userscript extension like  violent monkey
-* Open the extension settings, hit install from url
+* Install a userscript extension like Violent Monkey.
+* Open the extension settings, hit install from URL.
 * https://raw.githubusercontent.com/IceWreck/Page-Visibility-User-Script/master/pagevisibilityuserscript.js
 * Confirm Installation.
--- pagevisibilityuserscript.js
+++ pagevisibilityuserscript.js
@@ -2,22 +2,47 @@
 // @name        PreventPageVisibility 
 // @namespace   https://github.com/IceWreck
 // @match       *://*/*
+// @run-at      document-start
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      IceWreck
-// @description 9/16/2020, 12:27:16 AM
+// @description Block websites from knowing if you switched tabs/windows
+
 // ==/UserScript==
 
-for (event_name of ["visibilitychange", "webkitvisibilitychange", "blur"]) {
-  window.addEventListener(event_name, function(event) {
-        event.stopImmediatePropagation();
-    }, true);
+// This userscript blocks the page visibility API and to some extent the old blur/focus APIs.
+
+let events_to_block = [
+  "visibilitychange",
+  "webkitvisibilitychange",
+  "mozvisibilitychange",
+  "hasFocus",
+  "blur",
+  "focus",
+  "mouseleave"
+]
+
+for (event_name of events_to_block) {
+  document.addEventListener(event_name, function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      event.stopImmediatePropagation();
+  }, true);
 }
 
-document.hasFocus = function () {return true;};
-window.onfocus = null;
-window.onblur = null;
+for (event_name of events_to_block) {
+  window.addEventListener(event_name, function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      event.stopImmediatePropagation();
+  }, true);
+}
+
+
+document.hasFocus = function () { return true; };
 document.onvisibilitychange = null;
-Object.defineProperty(document, "hidden", { value : false});
-Object.defineProperty(document, "visibilityState", { value : "visible"});
-Object.defineProperty(document, "webkitVisibilityState", { value : "visible"});
+Object.defineProperty(document, "visibilityState", { value: "visible" });
+Object.defineProperty(document, "hidden", { value: false });
+Object.defineProperty(document, "mozHidden", { value: false });
+Object.defineProperty(document, "webkitHidden", { value: false });
+Object.defineProperty(document, "webkitVisibilityState", { value: "visible" });
